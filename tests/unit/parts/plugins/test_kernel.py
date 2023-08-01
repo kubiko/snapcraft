6f7fc654c50de93aae6185d5cfdcb86aab959edc
Ondrej Kubik <ondrej.kubik@canonical.com>
2023-08-02 00:10:13 +0300

    test: fix legacy(v2) kernel, initrd tests after kernel plugin split
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- tests/unit/parts/plugins/test_kernel.py
+++ tests/unit/parts/plugins/test_kernel.py
@@ -531,8 +531,8 @@ class TestPluginKernel:
                 filter(
                     None,
                     caplog.text.split(
-                        "WARNING  snapcraft.parts.plugins._kernel_build:_kernel_build.py"
-                    ),  # XXX: use a better way to check log messages
+                        "WARNING  snapcraft_legacy.plugins.v2._kernel_build:_kernel_build.py"
+                    ),
                 )
             )
             # there should be 2 warning logs, one for missing configs, one for kernel module
